0de51fd8c7e7a385f4501aaf1f54f2e708227bf5
Jamie Brown <jmbrowncow@gmail.com>
2018-10-02 17:13:00 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -20,6 +20,11 @@ class RepositoriesController < ApplicationController
   end
 
   # def create
+  #   response = Faraday.post 'https://api.github.com/user/repos' do |req|
+  #     req.body = { 'name': params[:name] }.to_json
+  #     req.headers['Authorization'] = 'token ' + session[:token]
+  #     req.headers['Accept'] = 'application/json'
+  #   end
+  #   redirect_to '/'
   # end
-
 end
